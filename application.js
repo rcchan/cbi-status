8c7312918ab74800fe95e329e321c428e9fde847
Ryan Chan <ryan@rcchan.com>
2016-07-15 17:54:18 -0400

    Initial commit

---
+++ application.js
@@ -0,0 +1,34 @@
+(function() {
+  var CBI_IMG_BASE = 'http://www.community-boating.org/wp-content/themes/communityboating/images/';
+  var flagImg = function(flagColor) {
+    return CBI_IMG_BASE + (function(flagColor) {
+      switch (flagColor) {
+        case "G":
+            return 'green-flag';
+        case "Y":
+            return 'yellow-flag';
+        case "R":
+            return 'red-flag';
+        default:
+            return 'burgee';
+      }
+    })(flagColor) + '.png';
+  };
+
+  chrome.contextMenus.create({
+    title: "CBI Website",
+    contexts: ["browser_action"],
+    onclick: function() {
+      window.open('http://community-boating.org');
+    }
+  });
+
+  (function updateBadge() {
+    $.get('https://portal2.community-boating.org/pls/apex/CBI_PROD.FLAG_JS', {}, function(r) {
+      chrome.browserAction.setIcon({
+        path: flagImg(Function(r + 'return FLAG_COLOR;')()),
+      });
+    });
+    setTimeout(updateBadge, 300000);
+  })();
+})();
